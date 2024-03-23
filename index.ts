3d316243d243eb18b5282a0be81574d3ecce5528
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-24 04:00:51 +0500

    Add files via upload
    this is my project

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+let answers = await inquirer.prompt([
+   {
+      name:"userGuessedNumber",
+      type:"number",
+      message:"Pleace guess a number between 1-10:",
+   },
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congtratulations! you guessed right number.")
+}
+else{
+    console.log("You guessed worng number")
+}
